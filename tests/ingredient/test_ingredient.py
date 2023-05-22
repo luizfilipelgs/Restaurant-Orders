90bc04a04a98eec18cc1d31a64e641d1c4d1ec8f
luizfilipelgs <luizfilipelgs@gmail.com>
2023-05-22 11:14:12 -0300

    feat: test class ingredient

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,20 @@
 from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+import pytest
 
 
 # Req 1
 def test_ingredient():
-    pass
+    ingredient = Ingredient("Sal")
+    assert ingredient.name == "Sal"
+    assert ingredient.restrictions == set()
+
+    ingredient1 = Ingredient("Sal")
+    ingredient2 = Ingredient("Sal")
+    ingredient3 = Ingredient("ovo")
+    assert ingredient1 == ingredient2
+    assert ingredient1 != ingredient3
+
+    assert hash(ingredient1) == hash(ingredient2)
+    assert hash(ingredient1) != hash(ingredient3)
+
+    assert repr(ingredient) == "Ingredient('Sal')"
